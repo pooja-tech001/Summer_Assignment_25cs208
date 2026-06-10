6053cee569a80e9fd628e133f14b0c442bd7ce48
pooja-tech001 <pooja.kumari2025@glbajajgroup.org>
2026-06-10 15:05:18 +0530

    Create q15.c

---
+++ day4/q15.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+    int num, sum = 0, rem, temp;
+    
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    temp = num;
+    
+    while(temp != 0) {
+        rem = temp % 10;
+        sum = sum + (rem * rem * rem);
+        temp = temp / 10;
+    }
+    
+    if(sum == num)
+        printf("%d is Armstrong number", num);
+    else
+        printf("%d is not Armstrong number", num);
+    
+    return 0;
+}
